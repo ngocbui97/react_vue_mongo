2c3202c9667ef825e5ad3c9536c4bf2895de44f7
KAKASHI\ngoc <buinhungoc97@gmail.com>
2020-12-09 20:32:20 +0700

    update pemiss

---
+++ APInetcore/TiketAPI/Extensions/SessionCoreExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiketAPI.Extensions
+{
+    public static class SessionCoreExtensions
+    {
+        public static void Add<T>(this ISession iSession, string key, T data)
+        {
+            string serializedData = JsonConvert.SerializeObject(data);
+            iSession.SetString(key, serializedData);
+        }
+        public static T Get<T>(this ISession iSession, string key)
+        {
+            var data = iSession.GetString(key);
+            if (null != data)
+                return JsonConvert.DeserializeObject<T>(data);
+            return default(T);
+        }
+    }
+}
